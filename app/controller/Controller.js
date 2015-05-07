46e8f4b69eee13f1944d4bd1a2f72559e21a462d
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-05-07 17:15:38 +0200

    pbi inserted to table

--- app/controller/Controller.js
+++ app/controller/Controller.js
@@ -22,10 +22,10 @@ Ext.define('PBI.controller.Controller', {
 				click: this.debugConsole
 			},
 			'createPbi > toolbar[id="createPbiToolbar"] > button[id="confirmBtn"]': {
-				click: this.debugConsole
+				click: this.insertPbi
 			},
 			'createPbi > toolbar[id="createPbiToolbar"] > button[id="resetBtn"]': {
-				click: this.debugConsole
+				click: this.resetPbi
 			},
 		});
 	},
@@ -37,5 +37,37 @@ Ext.define('PBI.controller.Controller', {
 	createPbi: function () {
 		createWindow = Ext.widget('createPbi');
 		createWindow.displayWindow();
+	},
+
+	insertPbi: function() {
+		if (Ext.getCmp('pbiId').isValid() && Ext.getCmp('pbiDescr').isValid()) {
+			var pbiId = Ext.getCmp('pbiId').getValue();
+			var pbiDescr = Ext.getCmp('pbiDescr').getValue();
+			Ext.Ajax.request({
+			    url: 'data/pbi_insert.php',
+			    method: 'POST',
+			    waitTitle: 'Connecting',
+			    waitMsg: 'Sending data...',
+			    params: {
+			        pbiId: pbiId,
+			        pbiDescr: pbiDescr
+			    },
+		    	success: function(error){
+                    var postResponse = Ext.decode(error.responseText);
+                    if (postResponse.success == "true") { 
+                        createWindow.close();
+                    } else {
+                        Ext.Msg.alert('Error', postResponse.error_code);
+                        Ext.getCmp('pbiId').reset();
+						Ext.getCmp('pbiDescr').reset();
+                    }
+                }
+			});
+		}
+	},
+
+	resetPbi: function() {
+		Ext.getCmp('pbiId').reset();
+		Ext.getCmp('pbiDescr').reset();
 	}
 });
--- data/pbi_insert.php
+++ data/pbi_insert.php
@@ -2,6 +2,9 @@
 // Get conf data
 include_once 'dbconf/dbconf.php';
 
+$pbi_id = $_POST['pbiId'];
+$pbi_descr = $_POST['pbiDescr'];
+
 // Create connection
 $conn = new mysqli($host, $user, $pwd, $dbname);
 // Check connection
@@ -9,13 +12,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$stmt = $mysqli->prepare("insert into pbi_list (pbi_id,pbi_descr) values (?,?)");
+$stmt = $conn->prepare("insert into pbi_list (pbi_id,pbi_descr) values (?,?)");
 $stmt->bind_param('is', $pbi_id, $pbi_descr);
 
 if ($stmt->execute()) {
     echo '{"success":"true"}';
 } else {
-	echo '{"success":"false"}';
+	$error_code = $stmt->error;
+	echo '{"success":"false","error_code":"'.$error_code.'"}';
 }
 
 mysqli_close($conn);
