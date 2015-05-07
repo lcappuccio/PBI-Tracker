85673ba95f5a785a0e59ec56354b3a5d23e8e7ac
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-05-07 13:27:01 +0200

    insert dao

---
+++ data/pbi_insert.php
@@ -0,0 +1,17 @@
+<?php
+// Get conf data
+include_once 'dbconf/dbconf.php';
+
+// Create connection
+$conn = new mysqli($host, $user, $pwd, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$stmt = $mysqli->prepare("insert into pbi_list (pbi_id,pbi_descr) values (?,?)");
+$stmt->bind_param('is', $pbi_id, $pbi_descr);
+$stmt->execute();
+
+mysqli_close($conn);
+?>
