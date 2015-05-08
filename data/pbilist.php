6493448e477c486c536e44afdc9adfb510c68967
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-05-08 11:30:14 +0200

    add date/timestamp to pbilist files

--- data/pbilist.php
+++ data/pbilist.php
@@ -9,7 +9,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "select pbi_id, pbi_descr, pbi_done_documentation, pbi_done_merge, pbi_done_validation_po, pbi_deployable, pbi_deployed from pbi_list";
+$sql = "select pbi_id, pbi_descr, pbi_done_documentation, pbi_done_merge, pbi_done_validation_po, pbi_deployable, pbi_deployed, pbi_insert_timestamp from pbi_list";
 $result = $conn->query($sql);
 // Print JSON
 $rows = array();
